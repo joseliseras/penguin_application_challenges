c0f3fdc37812fd6f4bb83ec167b8394f3212ebab
Ana <47386292+analiabritez@users.noreply.github.com>
2024-04-11 14:04:00 -0400

    Challenge 2 -  Sistema de Reservas en Línea

---
+++ challenge2.py
@@ -0,0 +1,13 @@
+# Eres parte de un equipo de desarrollo, su tarea es crear una aplicación web que permita a los clientes hacer reservas en un restaurante de manera fácil y eficiente.
+# El sistema debe manejar la disponibilidad de mesas, permitiendo a los usuarios seleccionar fechas y horarios.
+
+# Requisitos Específicos:
+# - Desarrollar la lógica para controlar la disponibilidad de mesas basada en la capacidad del restaurante y las reservas existentes.
+# - Implementar un sistema de gestión de reservas que permita crear, visualizar, modificar y cancelar reservas.
+# - Diseñar una página principal donde los clientes puedan seleccionar la fecha y hora de su reserva, así como el número de personas.
+# - Implementa formularios para la entrada de datos.
+
+# Tecnologías a utilizar:
+# - Python - Flask
+# - Tailwind CSS
+# - Base de datos a elección
